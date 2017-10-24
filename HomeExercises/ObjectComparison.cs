566380d2151ea8344034606fe505a82c584f94da
Di <mm.di@mail.ru>
2017-10-25 02:13:33 +0500

    test

--- HomeExercises/ObjectComparison.cs
+++ HomeExercises/ObjectComparison.cs
@@ -15,7 +15,7 @@ namespace HomeExercises
             var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
                 new Person("Vasili III of Russia", 28, 170, 60, null));
 
-// Перепишите код на использование Fluent Assertions.
+	// Перепишите код на использование Fluent Assertions.
             actualTsar.ShouldBeEquivalentTo(expectedTsar, options => options
                 .Excluding(x => x.Id)
                 .Excluding(x => x.Parent.Id));
